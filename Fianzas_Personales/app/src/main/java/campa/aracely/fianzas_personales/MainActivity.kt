6ecc2ff703d31fc12419bb5e3325dc9919b42b52
Jose Hinojosa <74486457+SrTohru@users.noreply.github.com>
2024-06-25 00:40:05 -0700

    Actualizacion de extras

--- Fianzas_Personales/app/src/main/java/campa/aracely/fianzas_personales/MainActivity.kt
+++ Fianzas_Personales/app/src/main/java/campa/aracely/fianzas_personales/MainActivity.kt
@@ -1,35 +1,72 @@
 package campa.aracely.fianzas_personales
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import campa.aracely.fianzas_personales.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        val graficas: Button = findViewById(R.id.graficas)
-        val login: Button = findViewById(R.id.Login)
-        val registroIngresos: Button = findViewById(R.id.registro_ingresos)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        graficas.setOnClickListener {
-            val intent = Intent(this, GraficasActivity::class.java)
-            startActivity(intent)
+        setSupportActionBar(binding.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+
+        drawerToggle = ActionBarDrawerToggle(
+            this, drawerLayout, binding.toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.inicioSesion -> {
+                startActivity(Intent(this, Activity_Login::class.java))
+            }
+            R.id.registro -> {
+                startActivity(Intent(this, ActivityRegistro::class.java))
+            }
+            R.id.registroIngresos -> {
+                startActivity(Intent(this, RegistroIngresosGastos::class.java))
+            }
         }
 
-        login.setOnClickListener {
-            val intent = Intent(this, Activity_Login::class.java)
-            startActivity(intent)
-        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
-        registroIngresos.setOnClickListener {
-            val intent = Intent(this, RegistroIngresosGastos::class.java)
-            startActivity(intent)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_inicio_drawer, menu)
+        return true
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
